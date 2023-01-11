cc46db2d624ce35db71ac4e83dc74f0d220fec66
MykhailoDymusha <mykhailo.dymusha@nure.ua>
2023-01-11 14:50:01 +0200

    Add Status code: 403 FORBIDDEN

--- src/main/java/com/pm/api/StatusCode.java
+++ src/main/java/com/pm/api/StatusCode.java
@@ -6,7 +6,8 @@ package com.pm.api;
 public enum StatusCode {
     SUCCESS(200),
     CREATED(201),
-    BAD_REQUEST(400);
+    BAD_REQUEST(400),
+    FORBIDDEN(403);
 
     private final int value;
 
